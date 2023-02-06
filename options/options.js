b0c934067e8fafa8135b558362dfb2ee28b1b8f5
Crowdbotics <engineering@crowdbotics.com>
2023-02-06 00:14:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-breeze-de-61343",
+  url: "https://lingering-breeze-de-61343.botics.co",
+  api: "https://lingering-breeze-de-61343.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
